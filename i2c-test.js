f2a901dd1eb527e1da742f78b5a9cb9fd3c6a2e2
Joerg Spoerl <joerg.spoerl@gmx.de>
2017-12-08 15:37:57 +0100

    add random example

--- i2c-test.js
+++ i2c-test.js
@@ -1,45 +1,119 @@
 const raspi = require('raspi');
 const I2C = require('raspi-i2c').I2C;
- 
-raspi.init(() => {
-  const i2c = new I2C();
+const gpio = require('raspi-gpio');
 
-  // console.log(i2c.writeByteSync(0x70,0,0xFF));
-  // console.log(i2c.writeByteSync(0x72,0,0xFF));
-  // console.log(i2c.writeByteSync(0x73,0,0xFF));
-  // console.log(i2c.writeByteSync(0x75,0,0xFF));
+/* ------------------------------------------------------------------ */
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-  // console.log(i2c.readByteSync(0x70));
-  // console.log(i2c.readByteSync(0x72));                                               
-  // console.log(i2c.readByteSync(0x74));                                               
-  // console.log(i2c.readByteSync(0x76));                                               
-  // console.log(i2c.readByteSync(0x78));                                               
+async function demo() {
+  console.log('Taking a break...');
+  await sleep(2000);
+  console.log('Two second later');
+}
+/* ------------------------------------------------------------------ */
 
-  // for (i = 0; i < 128; i++) {
-  //   console.log(i, i2c.readByteSync(i));
-  // }
+function randomIntInc (low, high) {
+  return Math.floor(Math.random() * (high - low + 1) + low);
+}
 
-  var i = 0
-  var interval = setInterval(() => {
-    //console.log(i, i2c.readByteSync(i));
+// raspi.init(() => {
+//   const output = new gpio.DigitalOutput('GPIO17');
+  
+//   //ouput.write(0);
 
-    i2c.writeByte(i,255, (err, data) => {
-      if (err)
-        console.log(i, " Error: ", err);
-      else
-        console.log(i, data)
-    })
+//   var o = false;
+//   var interval = setInterval (() => {
+//     o = !o;
+//     if (o)
+//       output.write(gpio.LOW);
+//     else
+//       output.write(gpio.HIGH);
     
+//   }, 2000)
+// })
 
-    i2c.readByte(i, (err, data) => {
+async function scan () {
+  raspi.init(async () => {
+    const i2c = new I2C();
+  
+    console.log("Start ...")
+    await sleep(1000);
+    console.log("---------")
+    
+    var i = 0
+  
+    // for (ziffer=0;ziffer<255;ziffer++) {
+    //   i2c.writeByte(i,1,ziffer, (err, data) => {
+    //     if (err)
+    //       console.log(i, ziffer, " Error: ", err);
+    //     else
+    //       console.log(i, ziffer, data)
+    //   })
+  
+    //   await sleep(500);
+    // }
+  
+    var interval = setInterval(async () => {
+      //console.log(i, i2c.readByteSync(i));
+      await sleep(100);
+      
+      // for (ziffer=0;ziffer<255;ziffer++) {
+      //   i2c.writeByte(i,1,ziffer, (err, data) => {
+      //     if (err)
+      //       console.log(i, " Error: ", err);
+      //     else
+      //       console.log(i, data)
+      //   })
+    
+      //   await sleep(100);
+      // }
+  
+      
+  
+      i2c.readByte(i, (err, data) => {
+        if (err)
+          console.log(i, " Error: ", err);
+        else
+          console.log(i, data)
+      })
+      i = i + 1;
+      if (i > 127)
+        clearInterval(interval);  
+    }, 300)
+  
+  });
+  
+}
+
+async function initSAA(address) {
+  const i2c = new I2C();
+  raspi.init(async () => {
+    i2c.writeByte(address, 0, 0b00110111, async (err, data) => {
       if (err)
-        console.log(i, " Error: ", err);
-      else
-        console.log(i, data)
-    })
-    i = i + 1;
-    if (i > 127)
-      clearInterval(interval);  
-  }, 300)
+        console.log(address, " Error: ", err);
+      else {
 
-});
+        while (true) {
+          console.log(address, "ok")
+          i2c.writeByteSync(address, 0, randomIntInc(0,255))
+          await sleep(randomIntInc(0,255));     
+          i2c.writeByteSync(address, 1, randomIntInc(0,255))
+          await sleep(randomIntInc(0,255));     
+          i2c.writeByteSync(address, 2, randomIntInc(0,255))     
+          await sleep(randomIntInc(0,255));     
+          i2c.writeByteSync(address, 3, randomIntInc(0,255))     
+          await sleep(randomIntInc(0,255));     
+          i2c.writeByteSync(address, 4, randomIntInc(0,255))     
+          await sleep(randomIntInc(0,255));     
+          }
+      }
+    })
+  })
+}
+
+
+//scan();
+
+initSAA(56);
