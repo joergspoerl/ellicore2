abf94080e577b9970432293e149fabfb3fd2761d
Joerg Spoerl <joerg.spoerl@gmx.de>
2017-12-09 17:46:53 +0100

    add SAA1064 string output -> ASCII table

--- index.ts
+++ index.ts
@@ -25,16 +25,46 @@ import { SAA_digit } from "./io/SAA1064"
 
 import { init as raspiInit }  from 'raspi';
 import { I2C }  from 'raspi-i2c';
+import { sleep } from "./helper/sleep";
 
 console.log(SAA_Address.first)
 
-raspiInit (()=> {
+raspiInit (async ()=> {
     const i2c = new I2C();
     const a = new SAA1064(i2c, SAA_Address.first);
 
-    a.ziffer(1,SAA_digit[10])
-    a.ziffer(2,SAA_digit[11])
-    a.ziffer(3,SAA_digit[12])
-    a.ziffer(4,SAA_digit[13])
+    // a.ziffer(1,SAA_digit[10])
+    // a.ziffer(2,SAA_digit[11])
+    // a.ziffer(3,SAA_digit[12])
+    // a.ziffer(4,SAA_digit[13])
+
+    // for (let i= 0; i<9999;i++) {
+    //     a.write(i.toString());
+    //     await sleep(10);
+    // }
+
+    // let array = [
+    //     "abcd",
+    //     "ef00",
+    //     "ABCD",
+    //     "ef  ",
+    //     "0000",
+    //     "1 2 ",
+    // ]
+    // while(true) {
+    //     for (let i = 0; i<array.length;i++) {
+    //         a.write(array[i]);
+    //         await sleep(1000);
+    //     }
+    // }
+
+    let longString: string = "abcdef01234567890  abcdef01234567890  abcdef01234567890  ";
+    while(true) {
+        for (let i = 0; i<longString.length;i++) {
+            a.write(longString.substring(i,i+4));
+            await sleep(1000);
+        }
+    }
+
 })
 
