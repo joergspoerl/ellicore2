2cbeee3c15fde626097f1e900aa37485f451c81c
Joerg Spoerl <joerg.spoerl@gmx.de>
2017-12-20 22:22:52 +0100

    add webservice functions

--- core/webservice.js
+++ core/webservice.js
@@ -4,7 +4,13 @@ var express = require('express');
 var app = express();
 var fs = require("fs");
 
-var bmv = require("../io/bmv");
+var victron_bmv = require("../io/victron_bmv");
+var bmv = new victron_bmv("/dev/ttyUSB0")
+
+var victron_mk2 = require("../io/victron_mk2");
+var mk2 = new victron_mk2("/dev/ttyUSB1")
+
+//var bmv = require("../io/bmv");
 //var tristar = require("../io/tristar.js");
 
 //app.get('/index.html', function (req, res) {
@@ -18,16 +24,21 @@ var bmv = require("../io/bmv");
 app.use(express.static('public'));
 
 app.get('/bmv', function (req, res) {
-    var bmvData = bmv();
-    // console.log(bmvData);
-    res.end(JSON.stringify(bmvData));
+    res.end(JSON.stringify(bmv.data));
 })
 
-app.get('/tristar', function (req, res) {
-    var tristarData = tristar();
-    // console.log(bmvData);
-    res.end(JSON.stringify(tristarData));
+app.get('/mk2', function (req, res) {
+    res.end(JSON.stringify(mk2.data));
 })
+app.get('/mk2/set_assist/:value', function (req, res) {
+    res.end( mk2.set_assist(req.params.value) );
+})
+
+// app.get('/tristar', function (req, res) {
+//     var tristarData = tristar();
+//     // console.log(bmvData);
+//     res.end(JSON.stringify(tristarData));
+// })
 
 
 var server = app.listen(8081, function () {
