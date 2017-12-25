d663a7e8275cda7a204fe3eceb33cf488754c764
Joerg Spoerl <joerg.spoerl@gmx.de>
2017-12-25 22:39:02 +0100

    saa1064 add write (str) function

--- io/saa1064.js
+++ io/saa1064.js
@@ -1,6 +1,6 @@
 // const raspi = require('raspi');
 // const I2C = require('raspi-i2c').I2C;
-
+const saa_ascii = require("./saa1064_ascii")
 
 function saa1064 (i2c, address) {
     var self = this;
@@ -30,6 +30,40 @@ function saa1064 (i2c, address) {
             });
         })
     }
+
+    self.write = (str) => {
+        return new Promise ( (resolve, reject) => {
+            var a = str.padStart(4, " ");
+            //let b = str.padEnd(4, " ")
+            var characters = a.split('');
+            var digitNumber = 1;
+            var currentChar = "";
+        
+            // console.log("a:", a);    
+            // console.log("a:", a);    
+            // console.log("characters:", characters);
+        
+            while (digitNumber <= 4 ){
+              currentChar = characters.shift();
+              while (currentChar == ".") {
+                currentChar = characters.shift()
+              }
+              // console.log("currentChar:", currentChar);
+              // console.log("digitNumber:", digitNumber);
+              if (characters[0] == ".") {
+                //characters.shift();
+                self.i2c.writeByteSync(self.address, digitNumber, saa_ascii[currentChar.charCodeAt(0)] + 0b10000000);
+              } else {
+                self.i2c.writeByteSync(self.address, digitNumber, saa_ascii[currentChar.charCodeAt(0)]);
+              }
+              digitNumber++
+            }
+            resolve ({
+                value: str
+            })
+        } )
+    }
+    
 
     return self
 }
