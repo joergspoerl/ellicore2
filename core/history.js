936d2878575ae74ea1046b1e3c9b588623de94de
Joerg Spoerl <joerg.spoerl@gmx.de>
2017-12-26 13:59:43 +0100

    history change array

--- core/history.js
+++ core/history.js
@@ -1,24 +1,74 @@
-
 const core = require("./core")
 
-var limit  = 20;
-var buffer = Array.apply(null, Array(limit)).map(Number.prototype.valueOf,0);
+var data = [
+    { 
+        buffer: new hbuffer (60)
+    },
 
+    { 
+        buffer: new hbuffer (60)
+    },
+    
+]
+
+
+// every second
 setInterval(() => {
-    var v = core.devices.bmv.data.V;
+    var value = core.devices.bmv.data.V;
 
-    if (v) {
-        buffer.push(v)
+    data[0].buffer.add(value)
 
-        if (buffer.length > limit) {
-            buffer.shift()
-        }
-    }
-    
-    
-
-    console.log("buffer", buffer)
-
+    //console.log("buffer", data)
 }, 1000);
 
-module.exports = buffer;
+
+
+// every minute
+setInterval(() => {
+
+//    var sum = data[0].buffer.sum()
+    var avg = data[0].buffer.avg()
+    console.log("sum", data[0].buffer.sum())
+
+    console.log("avg", avg)
+
+
+    data[1].buffer.add(avg)
+
+}, 10000);
+
+
+
+function hbuffer (size) {
+    this._size   = size
+    this._buffer = Array.apply(null, Array( size )).map( () => { return { value: 0, date: 0}});
+
+    this.add = function (value) {
+        if (value) {
+            this._buffer.push({
+                value: value,
+                date:  Date.now()
+            })
+
+            if (this._buffer.length > this._size) {
+                this._buffer.shift()
+            }
+        }
+    }
+
+    this.sum = function () {
+        return this._buffer.map( x => x.value ).reduce(function(a, b) { 
+            console.log("a, b", a, b)
+            return a + b
+        })
+    }
+
+    this.avg = function () {
+        return this.sum() / this._buffer.length;
+    }
+}
+
+
+
+
+module.exports = data;
