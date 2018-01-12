eaa7393210450799dfaa9fb63235810c8cf7be19
Joerg Spoerl <joerg.spoerl@gmx.de>
2018-01-12 18:30:47 +0100

    main remove old history code

--- core/main.js
+++ core/main.js
@@ -1,6 +1,6 @@
 ﻿// Main
 
 var core       = require("./core.js");
-var history    = require("./history.js");
+//var history    = require("./history.js");
 var webservice = require("./web/web.js");
 
