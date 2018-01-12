073f0cd80bdab298373aa237ab640eba59f40a54
Joerg Spoerl <joerg.spoerl@gmx.de>
2018-01-12 18:31:02 +0100

    add ssl

--- core/db/test-db.js
+++ core/db/test-db.js
@@ -1,26 +1,80 @@
-var Client = require('mariasql');
+const Client = require('mariasql');
+const http   = require('http');
+const https  = require('https');
+const url    = require('url')
 
 var c = new Client({
   host: '127.0.0.1',
   user: 'root',
-  password: 'raspberry'
+  password: 'raspberry',
+  db: "history"
 });
 
-var prep = c.prepare(
-    'insert into history.data (time, name, level, value) values (now(), :name, :level, :value) ');
-
-let i = 0
-setInterval ( () => {
-    i++
-    console.log("Insert: ", i)
-
-    c.query(prep({ name: 'test4js', level:0, value: Math.random() }), function(err, rows) {
+function iter_source (cb) {
+    var prep_source = c.prepare(
+        'select * from source ');
+    
+    c.query(prep_source({}), function(err, rows) {
         if (err)
             throw err;
         console.dir(rows);
-        });
     
-}, 100)
+        rows.some((row) => {
+            cb(row)
+        })
+    });    
+}
+
+
+
+function get_value (source) {
+    let u = url.parse(source.url);
+    let client = http
+
+    if (u.protocol == "https:"){
+        client = https;
+    }
+
+    client.get(u, (resp) => {
+        let data = ''
+
+        // A chunk of data has been recieved.
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });
+ 
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+            // console.log(JSON.parse(data));            
+            console.log(data);
+            insert_value(source, data)  
+        });
+ 
+    }).on("error", (err) => {
+    console.log("Error: " + err.message);
+    });
+
+}
+
+function insert_value (source, value) {
+
+    var prep = c.prepare(
+    'insert into history.data (time, source_id, level, value) values (now(), :source_id, :level, :value) ');
+
+    c.query(prep({ source_id: source.id, level:0, value: value }), function(err, rows) {
+        if (err)
+            throw err;
+        console.dir(rows);
+    });
+    
+}
+
+
+setInterval(() => {
+    iter_source ((source) => {
+        get_value(source)
+    })  
+}, 1000);
       
 
 c.end();
