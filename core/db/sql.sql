8bc684baad7e5b1cccd0703b238ddb0bdc59d428
Joerg Spoerl <joerg.spoerl@gmx.de>
2018-01-18 16:28:27 +0100

    sql.sql

--- core/db/sql.sql
+++ core/db/sql.sql
@@ -116,12 +116,19 @@ select * from history.data;
 
 show table status;
 
+
+
 /* disc usage */
-SELECT 
-    table_name AS `Table`, 
-    round(((data_length + index_length) / 1024 / 1024), 2) `Size (MB)` 
-FROM information_schema.TABLES 
-WHERE table_schema = "history";
+SELECT                                                                                                               
+    table_name AS `Table`,                                                                                           
+    table_rows AS rows,                                                                                              
+    round(((data_length + index_length) / 1024 / 1024), 2) `Size (MB)`                                               
+                                                                                                                     
+FROM information_schema.TABLES                                                                                       
+WHERE table_schema = "history"                                                                                       
+
+
+
 
 
 OPTIMIZE TABLE data;
