ea99e2292c3c0cf0accba725592276d70f17dcaa
Joerg Spoerl <joerg.spoerl@gmx.de>
2018-01-24 00:26:16 +0100

    enable cors

--- server/history/web/web.js
+++ server/history/web/web.js
@@ -1,7 +1,7 @@
 var app  = require('express')();
 var cors = require('cors')
 
-//app.use(cors())
+app.use(cors())
 
 // anything beginning with "/api" will go into this
 app.use('/api', require('./api/api.js'));
