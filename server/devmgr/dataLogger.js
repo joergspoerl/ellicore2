ed600963e73ccf9afecad860260025334d15652e
Joerg Spoerl <joerg.spoerl@gmx.de>
2019-02-13 21:10:05 +0100

    fix dataLogger

--- server/devmgr/dataLogger.js
+++ server/devmgr/dataLogger.js
@@ -1,4 +1,13 @@
 var log4js = require('log4js');
+log4js.configure({
+    appenders: {
+      out: { type: 'stdout' },
+      app: { type: 'file', filename: 'dataLogger.log', maxLogSize: 10485760, backups: 100, compress: true  }
+    },
+    categories: {
+      default: { appenders: [ 'out', 'app' ], level: 'debug' }
+    }
+  });
 var dataLogger = log4js.getLogger();
 dataLogger.level = 'debug';
 dataLogger.debug("First Message");
@@ -6,7 +15,7 @@ dataLogger.debug("First Message");
 var core = require("./devmgr.js");
 
 setInterval(() => {
-    dataLogger.debug("DATA: ", JSON.stringify(core.devices.bmv.data));
+    dataLogger.info("DATA: ", JSON.stringify(core.devices.bmv.data));
 }, 2000)
 
 
