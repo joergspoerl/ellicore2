fc88311f468828877bf1a3a6fb62677bf52008cc
Joerg Spoerl <joerg.spoerl@gmx.de>
2019-02-20 08:06:58 +0100

    change dataLoggoer to dateFile

--- server/devmgr/dataLogger.js
+++ server/devmgr/dataLogger.js
@@ -2,7 +2,7 @@ var log4js = require('log4js');
 log4js.configure({
     appenders: {
       out: { type: 'stdout' },
-      app: { type: 'file', filename: 'dataLogger.log', maxLogSize: 10485760, backups: 100, compress: true  }
+      app: { type: 'dateFile', filename: 'dataLogger.log', alwaysIncludePattern: true, keepFileExt: true,  compress: true }
     },
     categories: {
       default: { appenders: [ 'out', 'app' ], level: 'debug' }
@@ -15,7 +15,7 @@ dataLogger.debug("First Message");
 var core = require("./devmgr.js");
 
 setInterval(() => {
-    dataLogger.info("DATA: ", JSON.stringify(core.devices.bmv.data));
+    dataLogger.info("BMV: ", JSON.stringify(core.devices.bmv.data));
 }, 2000)
 
 
