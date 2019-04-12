7c3a0bd3b96e5ac74f58146de39e67473df23e79
Joerg Spoerl <joerg.spoerl@gmx.de>
2019-04-12 08:54:42 +0200

    dataLogger -> add path

--- server/devmgr/dataLogger.js
+++ server/devmgr/dataLogger.js
@@ -2,7 +2,7 @@ var log4js = require('log4js');
 log4js.configure({
     appenders: {
       out: { type: 'stdout' },
-      app: { type: 'dateFile', filename: 'dataLogger.log', alwaysIncludePattern: true, keepFileExt: true,  compress: true }
+      app: { type: 'dateFile', filename: 'log/dataLogger.log', alwaysIncludePattern: true, keepFileExt: true,  compress: true }
     },
     categories: {
       default: { appenders: [ 'out', 'app' ], level: 'debug' }
---
+++ server/devmgr/start-mqtt-autossh.sh
@@ -0,0 +1,1 @@
+screen autossh -R 0.0.0.0:1883:localhost:1883 ubuntu@ec2-18-196-22-3.eu-central-1.compute.amazonaws.com
