e2a3ade434e43dce92af607d94ff21c4777c4a26
joerg on pi <joerg.spoerl@gmx.de>
2020-09-26 21:29:11 +0200

    create test

---
+++ server/devmgr/test.js
@@ -0,0 +1,12 @@
+﻿console.log("Use 'core' module")
+
+
+var victron_mk2 = require("./io/victron_mk2");
+var mk2 = new victron_mk2("/dev/ttyUSB0")
+
+
+setTimeout( () => {
+    mk2.set_assist(16);
+}, 10000)
+
+
